34045a5e129f5008dd31c5d1772a02e50bd31b4b
lujanfernaud <lujanfernaud@gmail.com>
2019-01-07 09:53:43 +0000

    Update to apollo-server-express 2.3.1

--- server/server.js
+++ server/server.js
@@ -1,42 +1,43 @@
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { ApolloServer, gql } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const express = require('express');
-const expressJwt = require('express-jwt');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const expressJwt = require('express-jwt')
 const fs = require('fs')
-const jwt = require('jsonwebtoken');
-const db = require('./db');
+const jwt = require('jsonwebtoken')
+const db = require('./db')
 
-const port = 9000;
-const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
-
-const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'})
-const resolvers = require('./resolvers')
-const schema = makeExecutableSchema({typeDefs, resolvers})
+const port = 9000
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64')
 
 const app = express();
+
 app.use(cors(), bodyParser.json(), expressJwt({
   secret: jwtSecret,
   credentialsRequired: false
-}));
+}))
 
-app.use('/graphql', graphqlExpress((request) => ({
-  schema,
-  context: { user: request.user && db.users.get(request.user.sub) }
-})))
+const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf-8'}))
+const resolvers = require('./resolvers')
 
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+const graphqlServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => ({ user: req.user && db.users.get(req.user.sub) })
+})
+
+graphqlServer.applyMiddleware({app})
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
+  const {email, password} = req.body
+  const user = db.users.list().find((user) => user.email === email)
   if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
+    res.sendStatus(401)
+    return
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
-});
+  const token = jwt.sign({sub: user.id}, jwtSecret)
+  res.send({token})
+})
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+app.listen(port, () => console.info(`Server started on port ${port}`))
